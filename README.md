ef72f7d086a8ddf3d70b177c14f3bf91380839ab
DeadPackets <deadpackets@protonmail.com>
2020-04-06 18:18:02 +0400

    Final commit, project done

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # IsItBlockedInTheUae
-A simple NodeJS app (and CLI app!) that let's users figure out if a website has been blocked in the UAE.
+![GitHub](https://img.shields.io/github/license/DeadPackets/IsItBlockedInTheUae)
+![GitHub package.json version](https://img.shields.io/github/package-json/v/DeadPackets/IsItBlockedInTheUAE)
+![Project status](https://img.shields.io/badge/status-complete-success)
+
+A NodeJS project that allows you to programmatically detect domains that are blocked in the UAE (currently only configured for Etisalat users).
+
+# NOTICE!
+1. This program will only work on connections originating from inside the UAE (run the software from a device inside the UAE's network).
+2. Mass querying for blocked domains will likely get you flagged by analytic systems inside of the UAE, so use at your own discretion.
+
+## The web app
+Inside of `index.js` is an express web app that takes a port to listen on as an argument.
+
+Run it like this:
+```bash
+node index.js 8080 # Where 8080 is the port to listen on`
+```
+
+## The cli app
+Inside of `cli.js` is a command line version of the project that takes a file containing domains (each domain being on a new line) as argument. The program will save the blocked domains in a file in the same directory called `results.txt`.
+
+Run it like this:
+```bash
+node cli.js ./path_to_domain
+```
--- cli.js
+++ cli.js
@@ -1,1 +1,76 @@
 // TODO: Work on this
+/* eslint-disable no-console */
+const superagent = require('superagent');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+function isBlockedCheck(domain) {
+  return new Promise((resolve, reject) => {
+    // console.log(`Checking domain ${domain}`);
+    superagent
+      .get('http://proxy.etisalat.ae')
+      .set('Host', domain.trim())
+      .end((err, res) => {
+        if (err && !res) {
+          reject(err);
+        } else if (res.status !== 503 && res.status !== 403) {
+          if (res.status === 400) {
+            // Website is not blocked
+            resolve({ didWork: true, isBlocked: false });
+          } else {
+            reject(err);
+          }
+        } else {
+          // Check if the specific tag is there
+          const $ = cheerio.load(res.text);
+          if (
+            $('div.banner-text h3.english-on').text()
+            === 'This website is not accessible in the UAE.'
+          ) {
+            resolve({
+              didWork: true,
+              isBlocked: true,
+              category: res.text.match(
+                /This website was categorized as(?<ignore>.*?): (?<category>.*)/,
+              ).groups.category,
+            });
+          } else if ($('iframe[src="http://proxy.etisalat.ae/"]').length > 0) {
+            resolve({
+              didWork: true,
+              isBlocked: true,
+              category: 'null',
+              domain,
+            });
+          } else {
+            resolve({ didWork: true, isBlocked: false });
+          }
+        }
+      });
+  });
+}
+
+fs.readFile(process.argv[2], 'utf8', async (err, content) => {
+  if (err) {
+    throw err;
+  } else {
+    const domains = content.split('\n');
+    let count = 0;
+    console.log(`${domains.length} loaded.`);
+    for (let i = 0; i < domains.length; i++) {
+      try {
+        const result = await isBlockedCheck(domains[i]);
+        if (result.isBlocked) {
+          fs.appendFileSync('./results.txt', `${result.domain.trim()}\n`, 'utf8');
+          console.log(
+            `${result.domain.trim()} is blocked (${i})[${++count}/${
+              domains.length
+            }]!`,
+          );
+        }
+      } catch (e) {
+        // do nothing
+        // console.log(e);
+      }
+    }
+  }
+});
